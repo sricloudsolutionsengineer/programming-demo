f6a74f3e2fa7fb891b3b1c51f9a5f3a173119276
sricloudsolutionsengineer <sri.cloudsolutionsengineer@gmail.com>
2025-08-14 06:10:50 +0000

    Added new Java file

---
+++ JavaDemo.java
@@ -0,0 +1,8 @@
+public class JavaDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Java Demo1 ");
+         System.out.println("Java Demo1 ");
+          System.out.println("Java Demo1 ");
+    }
+}
